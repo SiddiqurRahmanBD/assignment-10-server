7fd18312255d8ef9da1b66f150fdfbf5f8dd7672
Siddiqur Rahman <mdsqkn@gmail.com>
2025-11-21 01:36:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri =
+  "mongodb+srv://food:68mPPW7TD0xfU5UZ@duasmasi.bhtinpf.mongodb.net/?appName=Duasmasi";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    //Database
+
+    const fooddb = client.db('fooddb');
+    const foodCollection = fooddb.collection("foods");
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Server Working!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
